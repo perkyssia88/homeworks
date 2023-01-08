6d1e07ed6e2deabedf09d1c9db709b84864f1e4e
Nikolay <119897180+perkyssia88@users.noreply.github.com>
2023-01-08 11:31:02 +0300

    Added homework lesson_16 (exeptions)

--- lesson_16/hw_lesson_16_Buka_PY_107.py
+++ lesson_16/hw_lesson_16_Buka_PY_107.py
@@ -29,3 +29,31 @@ def div2(a, b):
 
 num1, num2 = input("Ввод 1: "), input("Ввод 2: ")
 print(div2(num1, num2))
+
+""" Homework
+Ввод с клавиатуры. Если строка введенная с клавиатуры - числа, то поделить первое на второе.
+Обработать ошибку деления на ноль. Если второе число 0, то запросить ввод заново.
+Если введены буквы, то обработать исключение.
+"""
+
+
+def div_input_nums():
+    """
+    :return: asking for user input, str[0]/str[1],
+    handled exceptions ZeroDivisionError & ValueError
+    """
+    user_input = input("Введите строку из 2-х чисел раздеделенных пробелом: ")
+    lst = user_input.split(" ")
+    try:
+        div = int(lst[0]) / int(lst[1])
+    except ValueError:
+        return div_input_nums()
+    except ZeroDivisionError:
+        return div_input_nums()
+    except IndexError:
+        return div_input_nums()
+    else:
+        return f"Частное введенных чисел равно: {div}"
+
+
+print(div_input_nums())
